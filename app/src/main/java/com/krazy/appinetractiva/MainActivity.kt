4b1cfcaa13daf772598c4dbe8f85f2300291fd0d
KrazyLemon <angeleduvelazquez@gmail.com>
2024-06-21 09:53:11 -0600

    Initial commit

---
+++ app/src/main/java/com/krazy/appinetractiva/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.krazy.appinetractiva
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "screen_A",builder = {
+               composable("screen_A",){
+                   ScreenA(navController)
+               }
+                composable("screen_B"){
+                    ScreenB()
+                }
+            } )
+        }
+    }
+}
+
